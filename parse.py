c85b7b2cccf76085aaf66b666555d4d9a7f58caa
Ted Timmons <ted@perljam.net>
2014-10-07 21:03:40 -0700

    revise to add formulas and counts

--- parse.py
+++ parse.py
@@ -7,7 +7,9 @@ from collections import Counter
 req = requests.get('http://www.wrh.noaa.gov/pqr/climate/PDXtemp.txt')
 #print req.content
 
-print "year, days over 65, days over 70"
+print "year, days over 65, days over 70, days over 75"
+
+warm_years = 0
 
 for line in req.content.splitlines():
   year = str(line[:4])
@@ -25,5 +27,12 @@ for line in req.content.splitlines():
 
   #print "%s-%s xx %s" % (year, month, len(row))
 
-  print "%s, %s, %s" % (year, sum(int(x) >= 65 for x in row), sum(int(x) >= 70 for x in row))
+  over65 = sum(int(x) >= 65 for x in row)
+  over70 = sum(int(x) >= 70 for x in row)
+  over75 = sum(int(x) >= 75 for x in row)
+  print "%s, %s, %s, %s" % (year, over65, over70, over75)
 
+  if over65 >= 7 and over75 >= 2: warm_years += 1
+
+print "\nwarm years: %g" % warm_years
+print "(where warm year = a week above 65 and at least two days above 75)"
